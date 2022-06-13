2e634d713ce689b4d5621f8f2a5b9c98ecc46aed
Kaustubh P <kaustubhp.jsr@gmail.com>
2022-06-13 20:11:27 +0530

    fix: settings and migrations

--- wuphfer_backend/settings.py
+++ wuphfer_backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ga(t6m6ie!_x_uy@jhfeq0o%hnzl3q+vhrtd($c)hhkky(_tg!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
