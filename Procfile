4ebe2ed73ce02f15e1fffa523f7755c2b08d3b5d
Kaustubh P <kaustubhp.jsr@gmail.com>
2022-06-13 19:43:22 +0530

    Added procfile and heroku settings

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn wuphfer_backend.wsgi
--- wuphfer_backend/settings.py
+++ wuphfer_backend/settings.py
@@ -67,6 +67,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     "corsheaders.middleware.CorsMiddleware",
@@ -145,9 +146,38 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+try:
+    import os
+except:
+    pass
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    import django_heroku
+    django_heroku.settings(locals())
+except:
+    pass
+
+try:
+    import os
+    if 'DATABASE_URL' in os.environ:
+        import dj_database_url
+        DATABASES = {'default' : dj_database_url.config()}
+except:
+    pass
